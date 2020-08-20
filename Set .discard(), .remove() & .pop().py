a3294e250203194019ca3a601b654199847e4dc1
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-21 08:00:06 +0900

    Update Set .discard(), .remove() & .pop().py

--- Set .discard(), .remove() & .pop().py
+++ Set .discard(), .remove() & .pop().py
@@ -1,1 +1,16 @@
 # https://www.hackerrank.com/challenges/py-set-discard-remove-pop/problem
+n = int(input())
+s = set(map(int, input().split()))
+t = int(input())
+
+for i in range(t):
+    command = list(input().split())
+
+    if command[0] == 'discard':
+        s.discard(int(command[1]))
+    elif command[0] == 'remove':
+        s.remove(int(command[1]))
+    elif command[0] == 'pop':
+        s.pop()
+
+print(sum(s))
