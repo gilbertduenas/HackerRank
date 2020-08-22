853fc13911d22c50921cb8ebad9ab0dcf022b667
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-22 18:44:00 +0900

    Update Set .union() Operation.py

--- Set .union() Operation.py
+++ Set .union() Operation.py
@@ -1,1 +1,8 @@
 # https://www.hackerrank.com/challenges/py-set-union/problem
+
+i1 = input()
+set1 = set(map(int, input().split()))
+i2 = input()
+set2 = set1.union(map(int, input().split()))
+
+print(len(set2))
