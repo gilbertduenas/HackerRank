a0085736b4b18a178e776e818e1a9d446b0b4c11
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-23 11:22:06 +0900

    Update alternative_queries.sql

--- sql/alternative_queries.sql
+++ sql/alternative_queries.sql
@@ -1,17 +1,22 @@
 /* https://www.hackerrank.com/challenges/draw-the-triangle-1/problem */
+/*********************************************************************/
 SET @star = 21;
 
 SELECT REPEAT('* ', @star := @star - 1) 
 FROM information_schema.TABLES;
 
+
 /* https://www.hackerrank.com/challenges/draw-the-triangle-2/problem */
+/*********************************************************************/
 SET @star = 0;
 
 SELECT REPEAT('* ', @star := @star + 1)
 FROM information_schema.TABLES
 WHERE @star < 20;
 
+
 /* https://www.hackerrank.com/challenges/print-prime-numbers/problem */
+/*********************************************************************/
 SELECT GROUP_CONCAT(n1 SEPARATOR '&')
 FROM
   (SELECT @n1:=@n1+1 n1
