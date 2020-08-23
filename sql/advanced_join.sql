3fdbd3a2ad4e98e1d52cda58f1b304f11c3dc15d
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-24 08:20:49 +0900

    Update advanced_join.sql

--- sql/advanced_join.sql
+++ sql/advanced_join.sql
@@ -19,6 +19,23 @@ ORDER BY FP.salary;
 
 /* https://www.hackerrank.com/challenges/sql-projects/problem */
 /**************************************************************/
+SELECT start_date,
+       MIN(end_date)
+FROM
+  (SELECT start_date
+   FROM projects
+   WHERE start_date NOT IN
+       (SELECT end_date
+        FROM projects)) starts,
+
+  (SELECT end_date
+   FROM projects
+   WHERE end_date NOT IN
+       (SELECT start_date
+        FROM projects)) ends
+WHERE start_date < end_date
+GROUP BY start_date
+ORDER BY DATEDIFF(start_date, MIN(end_date)) DESC, start_date;
 
 
 /* https://www.hackerrank.com/challenges/symmetric-pairs/problem */
