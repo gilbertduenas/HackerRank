b03f87916ac29cfb3d7ddb3d542e6629a2d2b798
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-27 09:13:27 +0900

    Update advanced_join.sql

--- sql/advanced_join.sql
+++ sql/advanced_join.sql
@@ -40,4 +40,14 @@ ORDER BY DATEDIFF(start_date, MIN(end_date)) DESC, start_date;
 
 /* https://www.hackerrank.com/challenges/symmetric-pairs/problem */
 /*****************************************************************/
+SELECT f1.x,
+       f1.y
+FROM functions f1
+JOIN functions f2 ON f1.x = f2.y
+AND f1.y = f2.x
+GROUP BY f1.x,
+         f1.y
+HAVING COUNT(f1.x) > 1
+OR f1.x < f1.y
+ORDER BY f1.x;
 
