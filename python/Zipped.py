441c9bc5bac8b760ed9fe57862a3187d1bc8ef8c
gilbert <gilbertduenas@users.noreply.github.com>
2020-08-31 09:11:06 +0900

    Completed challenge.

--- python/Zipped.py
+++ python/Zipped.py
@@ -1,1 +1,11 @@
 # https://www.hackerrank.com/challenges/zipped/problem
+
+n, x = map(int, input().split())
+
+zipit = []
+
+for i in range(x):
+    zipit += [list(map(float, input().split()))]
+
+for j in zip(*zipit):
+    print(sum(j)/x)
