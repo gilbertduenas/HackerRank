1ab6c9f9cd52b35b82d45e7f1316e8b25a6814c9
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-05 13:32:20 +0900

    Last challenge for today.

--- python/Regex Substitution.py
+++ python/Regex Substitution.py
@@ -1,2 +1,13 @@
 # https://www.hackerrank.com/challenges/re-sub-regex-substitution/problem
 
+import re
+
+n = int(input())
+
+for i in range(n):
+    l = input()
+    
+    while ' && ' in l or ' || ' in l:
+        l = l.replace(' && ', ' and ').replace(' || ', ' or ')
+    
+    print(l)
