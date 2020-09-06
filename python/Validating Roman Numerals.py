5e438c2cdd60641b634062edc22ecb05b9bdca18
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-06 12:53:03 +0900

    Final fix.

--- python/Validating Roman Numerals.py
+++ python/Validating Roman Numerals.py
@@ -1,8 +1,11 @@
 # https://www.hackerrank.com/challenges/validate-a-roman-number/problem
 
+
+
 ones = '(?:(V?(I){0,3})|(IX)|(IV))?'
 tens = '(?:(L?(X){0,3})|(XC)|(XL))?'
 hundreds = '(?:(D?(C){0,3})|(CM)|(CD))?'
+thousands = '(?:(M){0,3})?'
 
 regex_pattern = r'^' + thousands + hundreds + tens + ones + '$'
 
