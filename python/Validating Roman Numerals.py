8b2f23cd658cb784a3047ec85d3da05b8e9746d9
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-06 12:48:51 +0900

    Adding 1 challenge.

--- python/Validating Roman Numerals.py
+++ python/Validating Roman Numerals.py
@@ -1,3 +1,11 @@
 # https://www.hackerrank.com/challenges/validate-a-roman-number/problem
 
-import re 
+import re
+
+ones = '(?:(V?(I){0,3})|(IX)|(IV))?'
+tens = '(?:(L?(X){0,3})|(XC)|(XL))?'
+hundreds = '(?:(D?(C){0,3})|(CM)|(CD))?'
+
+numeral = r'^' + thousands + hundreds + tens + ones + '$'
+
+print(str(bool(re.match(numeral, input()))))
