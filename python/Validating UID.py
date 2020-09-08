d21f510e7b17a2e477c25cdc7816f2bbd1f7e8ae
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-09 08:00:20 +0900

    Adding 1 challenge.

--- python/Validating UID.py
+++ python/Validating UID.py
@@ -1,2 +1,13 @@
 # https://www.hackerrank.com/challenges/validating-uid/problem
 
+import collections
+
+n = int(input())
+
+for i in range(n)
+    UID = str(input())
+    
+    if set(UID) < len(10):
+        print('Invalid)
+        return False
+        
