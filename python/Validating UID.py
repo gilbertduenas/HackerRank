cd6d917d24029990c08c6774ecce8e504820c714
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-10 07:59:21 +0900

    Add 1 challenge.

--- python/Validating UID.py
+++ python/Validating UID.py
@@ -1,13 +1,21 @@
 # https://www.hackerrank.com/challenges/validating-uid/problem
 
-import collections
+import re
 
 n = int(input())
 
-for i in range(n)
-    UID = str(input())
-    
-    if set(UID) < len(10):
-        print('Invalid)
-        return False
+for i in range(n):
+    UID = ''.join(sorted(input()))
+
+    try:
+        assert re.search(r'[A-Z]{2}', UID)
+        assert re.search(r'\d\d\d', UID)
+        assert not re.search(r'[^a-zA-Z0-9]', UID)
+        assert not re.search(r'(.)\1', UID)
+        assert len(UID) == 10
+
+    except:
+        print('Invalid')
         
+    else:
+        print('Valid')
