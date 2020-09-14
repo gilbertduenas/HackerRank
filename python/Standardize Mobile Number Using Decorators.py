db559f53a981b155a7d4d6176d996eecbb661c72
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-14 18:48:18 +0900

    Adding 1 challenge.

--- python/Standardize Mobile Number Using Decorators.py
+++ python/Standardize Mobile Number Using Decorators.py
@@ -1,2 +1,16 @@
 # https://www.hackerrank.com/challenges/standardize-mobile-number-using-decorators/problem
 
+def wrapper(f):
+    def fun(l):
+    
+# I just added this line for this challenge....
+        f(['+91 '+c[-10:-5]+' '+c[-5:] for c in l])
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
