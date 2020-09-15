f066436e9ce4bfd267641b140e61d22561f4e8b3
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-16 08:25:31 +0900

    Adding last challenge for the day.

--- python/Concatentate.py
+++ python/Concatentate.py
@@ -1,2 +1,10 @@
 # https://www.hackerrank.com/challenges/np-concatenate/problem
 
+import numpy
+
+n, m, p = map(int, input().split())
+
+array1 = [list(map(int, input().split())) for i in range(n)]
+array2 = [list(map(int, input().split())) for i in range(m)]
+
+print(numpy.concatenate((array1, array2), axis=0))
