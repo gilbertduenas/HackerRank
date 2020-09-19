fddc33ae12ccaec9a5ec26c38a9a934309c43494
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-19 15:04:57 +0900

    Adding 3 challenge updates.

--- python/Floor, Ceil, and Rint.py
+++ python/Floor, Ceil, and Rint.py
@@ -1,2 +1,10 @@
 # https://www.hackerrank.com/challenges/floor-ceil-and-rint/problem
 
+import numpy
+
+numpy.set_printoptions(sign=' ')
+n = list(map(float, input().split()))
+
+print(numpy.floor(n))
+print(numpy.ceil(n))
+print(numpy.rint(n))
--- python/Zeros and Ones.py
+++ python/Zeros and Ones.py
@@ -6,3 +6,4 @@ n = list(map(int, input().split()))
 
 print(numpy.zeros(n, dtype = numpy.int))
 print(numpy.ones(n, dtype = numpy.int))
+
