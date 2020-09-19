560ebea767ce2feec5b0a16ec9ef13711b51cb45
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-19 18:58:36 +0900

    Adding final challenge for the day

--- python/Min and Max.py
+++ python/Min and Max.py
@@ -1,1 +1,14 @@
 # https://www.hackerrank.com/challenges/np-min-and-max/problem
+
+import numpy
+
+n, m = map(int, input().split())
+l = []
+
+for i in range(n):
+    temp = list(map(int, input().split()))
+    l.append(temp)
+
+min_result = numpy.min(l, axis=1)
+
+print(numpy.max(min_result))
