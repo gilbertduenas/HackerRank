8db619ef93fc4d5fc6616e6cd45dcbe2793871ab
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-21 08:10:14 +0900

    challenge

--- python/Inner and Outer.py
+++ python/Inner and Outer.py
@@ -1,2 +1,9 @@
 # https://www.hackerrank.com/challenges/np-inner-and-outer/problem
 
+import numpy
+
+n = list(map(int, input().split()))
+m = list(map(int, input().split()))
+
+print(numpy.inner(n, m))
+print(numpy.outer(n, m))
