5c8f73c34c1e6ded6d7ad450afa6e112e2daae8c
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-22 18:47:37 +0900

    more

--- python/The Minion Game.py
+++ python/The Minion Game.py
@@ -1,2 +1,24 @@
 # https://www.hackerrank.com/challenges/the-minion-game/problem
 
+def minion_game(s):
+    v = 'AEIOU'
+    kev = 0
+    stu = 0
+
+    for i in range(len(s)):
+        if s[i] in v:
+            kev += (len(s)-i)
+        else:
+            stu += (len(s)-i)
+
+    if kev > stu:
+        print('Kevin', kev)
+    elif kev < stu:
+        print('Stuart', stu)
+    else:
+        print('Draw')
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
+    
