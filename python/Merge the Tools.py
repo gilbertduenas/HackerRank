423a256cd71ae12ae71aa19cab9679232c6a29ab
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-22 19:04:35 +0900

    more padding

--- python/Merge the Tools.py
+++ python/Merge the Tools.py
@@ -1,2 +1,19 @@
 # https://www.hackerrank.com/challenges/merge-the-tools/problem
 
+def merge_the_tools(s, k):
+    t = []
+    count = 0
+
+    for i in s:
+        count += 1
+        if i not in t:
+            t.append(i)
+        if count == k:
+            print(''.join(t))
+            t = []
+            count = 0
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
+    
