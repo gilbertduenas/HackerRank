4f8072fc8983ee618cbbca6ac3a72bcc4ceb690d
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-25 08:23:35 +0900

    morning challenge

--- python/Compress the String.py
+++ python/Compress the String.py
@@ -1,3 +1,11 @@
 # https://www.hackerrank.com/challenges/compress-the-string/problem
 
+from itertools import groupby
+
+l =[]
+
+for k, c in groupby(input()):
+    l.append((len(list(c)), int(k)))
+
+print(' '.join(map(str, l)))
 
