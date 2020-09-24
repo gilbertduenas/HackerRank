dbeba32af98453c0992f98b6345286899af98b17
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-24 19:43:19 +0900

    night challenge

--- python/Word Order.py
+++ python/Word Order.py
@@ -1,2 +1,18 @@
 # https://www.hackerrank.com/challenges/word-order/problem
 
+from collections import OrderedDict
+
+n = int(input())
+d = OrderedDict()
+
+for i in range(n):
+    k = input()
+
+    if not k in d.keys():
+        d.update({k:1})
+        continue
+    
+    d[k] += 1
+
+print(len(d.keys()))
+print(*d.values())
