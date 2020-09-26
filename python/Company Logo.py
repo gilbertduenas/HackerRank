b55663debd90476b88b91bcf250b61e77b7d1d8f
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-26 10:57:11 +0900

    it's raining right now

--- python/Company Logo.py
+++ python/Company Logo.py
@@ -1,2 +1,19 @@
 # https://www.hackerrank.com/challenges/most-commons/problem
 
+import math
+import os
+import random
+import re
+import sys
+
+from collections import Counter, OrderedDict
+
+class OrderedCounter(Counter):
+    pass
+
+if __name__ == '__main__':
+    s = sorted(input())
+
+    for c in OrderedCounter(s).most_common(3):
+        print(f'{c[0]} {c[1]}')
+        
--- python/Iterables and Iterators.py
+++ python/Iterables and Iterators.py
@@ -1,2 +1,12 @@
 # https://www.hackerrank.com/challenges/iterables-and-iterators/problem
 
+from itertools import combinations
+
+n = input()
+N = input().split()
+K = int(input())
+
+c = list(combinations(N, K))
+f = filter(lambda i: 'a' in i, c)
+
+print(len(list(f))/len(c))
