3dd53ef6bf65b1030b1a4af0099ecc34711d6070
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-27 11:39:59 +0900

    filler

--- python/Piling Up.py
+++ python/Piling Up.py
@@ -1,2 +1,20 @@
 # https://www.hackerrank.com/challenges/piling-up/problem
 
+from collections import deque
+
+n = int(input())
+
+for i in range(n):
+    m = input()
+    l = deque(map(int, input().split()))
+
+    for c in reversed(sorted(l)):
+        if l[-1] == c: 
+            l.pop()
+        elif l[0] == c: 
+            l.popleft()
+        else:
+            print('No')
+            break
+    else: print('Yes')
+
