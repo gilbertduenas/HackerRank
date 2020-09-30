9bb16adc905233b4bb5a5af19ba02fb474bcc65f
gilbert <gilbertduenas@users.noreply.github.com>
2020-10-01 07:41:32 +0900

    morning commit

--- python/Validating Credit Card Numbers.py
+++ python/Validating Credit Card Numbers.py
@@ -1,2 +1,19 @@
 # https://www.hackerrank.com/challenges/validating-credit-card-number/problem
 
+import re
+
+n = int(input())
+check = re.compile(
+    r"^"
+    r"(?!.*(\d)(-?\1){3})"
+    r"[456]"
+    r"\d{3}"
+    r"(?:-?\d{4}){3}"
+    r"$")
+    
+for i in range(n):
+    if check.search(input().strip()):
+        print('Valid')
+        
+    else: 
+        print('Invalid')
