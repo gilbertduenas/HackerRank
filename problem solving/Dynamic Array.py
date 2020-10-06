6bb4ad440b591f5239b00ff1e43d45ff98936a29
gilbert <gilbertduenas@users.noreply.github.com>
2020-10-07 07:52:54 +0900

    morning!

--- problem solving/Dynamic Array.py
+++ problem solving/Dynamic Array.py
@@ -1,1 +1,47 @@
 # https://www.hackerrank.com/challenges/dynamic-array/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def dynamicArray(n, queries):
+    seq = [[] for _ in range(n)]
+    last = 0
+    result = []
+
+    for q in queries:
+        index = (q[1] ^ last) % n
+        
+        if q[0] == 1:
+            seq[index].append(q[2])
+        else:
+            position = q[2] % len(seq[index])
+            last = seq[index][position]
+            result.append(last)
+
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = dynamicArray(n, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
