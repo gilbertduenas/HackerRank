aa5d4c731377c273b50eef0323b2c1083e8a8ac1
gilbert <gilbertduenas@users.noreply.github.com>
2020-10-10 16:42:20 +0900

    ahhhhh easy challenges

--- problem solving/Bill Division.py
+++ problem solving/Bill Division.py
@@ -1,1 +1,31 @@
 # https://www.hackerrank.com/challenges/bon-appetit/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+  total = b - (sum(bill) - bill[k])/2
+
+  if total == 0:
+    print('Bon Appetit')
+  else:
+    print(int(total))
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
