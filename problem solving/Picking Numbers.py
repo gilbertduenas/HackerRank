aa926da760879e477ca6fe666a3e45703d53aadd
gilbert <gilbertduenas@users.noreply.github.com>
2020-10-19 19:16:38 +0900

    one more touch

--- problem solving/Picking Numbers.py
+++ problem solving/Picking Numbers.py
@@ -8,13 +8,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'pickingNumbers' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY a as parameter.
-#
-
 def pickingNumbers(a):
     # Write your code here
 
