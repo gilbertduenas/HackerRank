17249caa10babbec38182ad6ac7344dbb649d719
gilbert <gilbertduenas@users.noreply.github.com>
2020-11-28 09:59:30 +0900

    back to it

--- problem solving/Utopian Tree.py
+++ problem solving/Utopian Tree.py
@@ -1,1 +1,35 @@
 # https://www.hackerrank.com/challenges/utopian-tree/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the utopianTree function below.
+def utopianTree(n):
+    c = 1
+    
+    for i in range(n):
+        if i % 2==0:
+            c *= 2
+        else:
+            c += 1
+            
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
