feb6c7b8922c651cfe784c0f8a1d3c5e21cb1c62
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-04 12:06:37 +0900

    Update Sequence Equation.py

--- problem solving/Sequence Equation.py
+++ problem solving/Sequence Equation.py
@@ -1,2 +1,27 @@
 # https://www.hackerrank.com/challenges/permutation-equation/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the permutationEquation function below.
+def permutationEquation(p):
+    return [p.index(p.index(i+1)+1)+1 for i in range(len(p))]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
