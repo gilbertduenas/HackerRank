61eb0515e1159ba4c6d8d1d4541205a13ab4a2a6
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-06 08:17:45 +0900

    Update Find Digits.py

--- problem solving/Find Digits.py
+++ problem solving/Find Digits.py
@@ -1,1 +1,27 @@
 # https://www.hackerrank.com/challenges/find-digits/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the findDigits function below.
+def findDigits(n):
+    return len([i for i in map(int, list(str(n))) if i != 0 and n % i == 0])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
