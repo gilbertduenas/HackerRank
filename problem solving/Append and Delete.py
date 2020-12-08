7e463855c39eb622b70889d75507108572da9c5e
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-08 19:19:37 +0900

    Update Append and Delete.py

--- problem solving/Append and Delete.py
+++ problem solving/Append and Delete.py
@@ -10,6 +10,17 @@ import sys
 
 # Complete the appendAndDelete function below.
 def appendAndDelete(s, t, k):
+    i = 0
+    
+    while i<len(s) and i<len(t) and s[i]==t[i]:
+        i+=1
+        
+    if (len(s)+len(t)-2*i)>k:
+        return 'No'
+    elif (len(s)+len(t)-k)<0 or (len(s)+len(t)-2*i)%2 == k%2:
+        return 'Yes'
+    else:
+        return 'No'
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
