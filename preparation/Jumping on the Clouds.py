26440c3d0fa86631be4d31d264970f542a2cc654
gilbert <gilbertduenas@users.noreply.github.com>
2021-01-19 08:34:52 +0900

    and 1

--- preparation/Jumping on the Clouds.py
+++ preparation/Jumping on the Clouds.py
@@ -1,1 +1,37 @@
 # https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    count = 0
+    i = 0
+    
+    while (i < n-1):
+        if (c[i] == 0): 
+            i += 2
+        else:   
+            i += 1
+            
+        count+=1
+        
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
